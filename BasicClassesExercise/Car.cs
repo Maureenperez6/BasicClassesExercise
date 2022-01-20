395c71168893cc6cde85317887722ba523a4e62a
Maureen Perez <maureen8694@gmail.com>
2022-01-20 12:23:36 -0500

    initial commit

---
+++ BasicClassesExercise/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClassesExercise
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+        public Car(string Make)
+        {
+            Make = Make;
+  
+        }
+     
+        public Car(int Year)
+
+        {
+            Year = Year;
+        }
+
+        public Car(string Model, bool IsTheLatestModel)
+        {
+            Model = Model;
+            IsTheLatestModel = IsTheLatestModel;
+        }
+
+        int year;
+        public string Model {get; set;}
+        public bool IsTheLatestModel { get; set;}
+        public string Make {get; set;}
+        public int Year {get; set;}
+       
+        public void MyNewCar()
+        {
+            Console.WriteLine($"My New Car is a {Year} {Make} {Model}, it {IsTheLatestModel}!");
+        }
+    }
+}
+
+
+
---
+++ BasicClassesExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicClassesExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.IsTheLatestModel = true;
+            myCar.Make = "Toyota";
+            myCar.Model = "4Runner";
+            myCar.Year = 2021;
+
+            myCar.MyNewCar();
+
+
+
+        }
+    }
+}
